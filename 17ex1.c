e2aea943bc9e1d7fc112d7d141f47c859600bd8b
trungnt1270 <71129889+trungnt1270@users.noreply.github.com>
2020-10-08 09:36:04 +0700

    lab10

---
+++ 17ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main1(int argc, char *argv[]) {
+	char name[20];
+	system("cls");
+	puts("Enter your name : ");
+	gets(name);
+	puts("Hi there : ");
+	puts(name);
+	getch();
+}
